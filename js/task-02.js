5e371391c8a7bd72ec54a14683e68e96875b507c
Shuster Oleksandr <godzilasanya@gmail.com>
2023-10-18 07:13:41 +0300

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+//створення масиву з li
+const ingredientsToItem = ingredients.map((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  return li;
+});
+//додоавання едементів з масиву
+const list = document.querySelector("#ingredients");
+list.append(...ingredientsToItem);
