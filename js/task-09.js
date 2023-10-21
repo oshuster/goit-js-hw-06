8baa2beda2d578bc76d052d84ac04c570fc8cdbe
Shuster Oleksandr <godzilasanya@gmail.com>
2023-10-21 08:17:11 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const output = document.querySelector(".color");
+const body = document.querySelector("body");
+btn.addEventListener("click", changeColor);
+output.textContent = "#fafafa";
+
+function changeColor() {
+  const color = getRandomHexColor();
+  output.textContent = color;
+  body.style.backgroundColor = color;
+}
