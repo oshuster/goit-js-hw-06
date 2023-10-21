4307ff0e2e1e6faf92f5a795e65589379d5129a5
Shuster Oleksandr <godzilasanya@gmail.com>
2023-10-21 08:04:51 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+//отримання доступу до форми
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Відсутні обов'язкові поля");
+  } else {
+    console.log({ email, password });
+    form.reset();
+  }
+}
