e6caacc7e3bdad589bf5f8f8bcb02bc1f74db061
Shuster Oleksandr <godzilasanya@gmail.com>
2023-10-28 17:00:00 +0300

    fix empty class

--- js/task-02.js
+++ js/task-02.js
@@ -10,6 +10,7 @@ const ingredients = [
 const ingredientsToItem = ingredients.map((item) => {
   const li = document.createElement("li");
   li.textContent = item;
+  li.classList.add("item");
   return li;
 });
 //додоавання елементів до масиву
